593d07ef49845a3d1d42234e9de7f809d318b7fa
Maximilian Sander <maximilian_sander@mailbox.org>
2023-10-01 22:41:31 +0200

    Remove not needed lines

--- tests/test_ddl.py
+++ tests/test_ddl.py
@@ -26,24 +26,3 @@ def test_mpi_program():
         process.terminate()
         process.wait()
         raise e
-
-# @pytest.mark.timeout(10)
-# def test_mpi_program():
-#     # Start the MPI program.
-#     cmd = ['mpirun', '-np', '4', 'python3', 'run_ddl.py']
-#     process = subprocess.Popen(cmd)
-#
-#     try:
-#         # Run your tests here.
-#         process.wait()
-#         rt = process.returncode
-#         print(rt)
-#         assert rt == 0
-#
-#     except Exception as e:
-#         # If any exception occurs (including a timeout), terminate the process.
-#         process.terminate()
-#         process.wait()
-#         raise e
-#
-#
